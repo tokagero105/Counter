750fc714890d6091ed9102f506b70dcdba28ad21
Semyon Titov <tokagero105@yandex.ru>
2023-07-09 15:44:01 +0300

    Initial Commit

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var plusCounterButton: UIButton!
+    @IBOutlet weak var minusCounterButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterResetButton: UIButton!
+    var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        counterLabel.text = String(counter)
     }
-
-
+    
+    @IBAction func plusButtonTouch(_ sender: Any) {
+        counter += 1
+        counterLabel.text = String(counter)
+    }
+    
+    @IBAction func minusButtonTouch(_ sender: Any) {
+        counter = counter - 1
+        if counter < 0 {
+            counter = 0
+        }
+        counterLabel.text = String(counter)
+    }
+    
+    @IBAction func counterResetTouch(_ sender: Any) {
+        counter = 0
+        counterLabel.text = String(counter)
+    }
 }
 
